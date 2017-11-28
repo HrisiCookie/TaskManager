deefb84ddbb6b869d71def82ded45932358d9e21
Hristina Bailova <hristina.bailova@gmail.com>
2017-11-28 17:18:42 +0200

    get data from Core Data and load it in cells

--- TaskManager/ViewControllers/MainViewController.swift
+++ TaskManager/ViewControllers/MainViewController.swift
@@ -7,24 +7,48 @@
 //
 
 import UIKit
+import CoreData
+
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class MainViewController: UIViewController {
+    
     // outlets
     @IBOutlet weak var tableView: UITableView!
+    
+    var tasks: [Task] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-//        tableView.isHidden = true
         
         self.tableView.register(UINib(nibName:"\(TaskCell.self)", bundle: nil), forCellReuseIdentifier: "\(TaskCell.self)")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.fetch { (complete) in
+            if complete {
+                if tasks.count < 1 {
+                    tableView.isHidden = true
+                } else {
+                    tableView.isHidden = false
+                }
+            }
+        }
+        tableView.reloadData()
     }
     
     // actions
     @IBAction func pressedAddTaskButton(_ sender: Any) {
         print("Pressed add task")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let addNewTaskVC = storyboard.instantiateViewController(withIdentifier: "\(AddNewTaskViewController.self)") as? AddNewTaskViewController else {return}
+        navigationController?.pushViewController(addNewTaskVC, animated: true)
     }
+    
     @IBAction func pressedSettingsButton(_ sender: Any) {
         print("Pressed settings")
     }
@@ -36,14 +60,38 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let taskCell = tableView.dequeueReusableCell(withIdentifier: "\(TaskCell.self)", for: indexPath) as? TaskCell else {return UITableViewCell()}
+        let currentTask = tasks[indexPath.row]
         
-        taskCell.configCell(taskTitle: "New task title", completionDate: "23.12.2017")
+//        taskCell.configCell(taskTitle: "New task title", completionDate: "23.12.2017")
+        taskCell.configCell(taskTitle: currentTask.taskTitle!, completionDate: currentTask.completionDate!)
         
         return taskCell
+    }
+}
+
+extension MainViewController {
+    // fetch all tasks data and save it in array
+    
+    func fetch(completion: (_ complete: Bool) -> ()) {
+        // fetch request - grab data from persistant storage
+        
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else {return}
+        
+        // fetch items that are in this entity
+        let fetchRequest = NSFetchRequest<Task>(entityName: "Task")
+        do {
+            tasks = try managedContext.fetch(fetchRequest)
+            print("Tasks array: \(tasks.count)")
+            print("Successfully fatched data!")
+            completion(true)
+        } catch {
+            print("Could not fetch: \(error.localizedDescription)")
+            completion(false)
+        }
     }
 }
